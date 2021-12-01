b798598bd14d274ad89a1cef386d5dec476fa3c3
arminasi <armanminasyanc@gmail.com>
2021-12-01 13:31:39 +0400

    Working with variables

---
+++ script.js
@@ -0,0 +1,11 @@
+//Working with variables
+
+    // Declare two variables: admin and name.
+        let admin;
+        let fname;
+
+    //Write the string "John" to the name variable.
+        fname = "John";
+
+    //Copy the value from the name variable to admin.
+        admin = fname;
